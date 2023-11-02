e75d6085c167ea421aa2119d62a2b2a419639785
Xeotroid <renda.fiala@seznam.cz>
2023-11-02 18:03:21 +0100

    Fix: Sentence case

--- BackEnd/StrExtensions.cs
+++ BackEnd/StrExtensions.cs
@@ -9,7 +9,7 @@ namespace BackEnd {
     static class StrExtensions {
         public static string FirstUpper(this string str) {
             if (string.IsNullOrEmpty(str)) return str;
-            return str.Substring(0, 1).ToUpper() + str.Substring(1);
+            return str.Substring(0, 1).ToUpper() + str.Substring(1).ToLower();
         }
     }
 }
